0bc1947a5c6d740b45be22d22f363ce6daf874f5
Michał Woś <mw792829570@gmail.com>
2015-11-01 22:00:08 +0100

    init

---
+++ src/main/java/pl/edu/mimuw/students/wosiu/scraper/selectors/Config.java
@@ -0,0 +1,4 @@
+package pl.edu.mimuw.students.wosiu.scraper.selectors;
+
+public class Config {
+}
---
+++ src/main/java/pl/edu/mimuw/students/wosiu/scraper/selectors/Selector.java
@@ -0,0 +1,10 @@
+package pl.edu.mimuw.students.wosiu.scraper.selectors;
+
+import java.net.URI;
+import java.util.List;
+
+public class Selector {
+	String country;
+	List<URI>proxies;
+
+}
