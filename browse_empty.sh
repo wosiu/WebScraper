54ca2e2afe3b388ba156efcb4ea4f4b000d4f423
Michał Woś <mw792829570@gmail.com>
2015-11-18 02:11:02 +0100

    empty browser script + fix

---
+++ browse_empty.sh
@@ -0,0 +1,1 @@
+java -classpath target/scraper-1.0-SNAPSHOT-jar-with-dependencies.jar pl.edu.mimuw.students.wosiu.scraper.BrowserOpener
--- src/main/java/pl/edu/mimuw/students/wosiu/scraper/BrowserOpener.java
+++ src/main/java/pl/edu/mimuw/students/wosiu/scraper/BrowserOpener.java
@@ -1,6 +1,8 @@
 package pl.edu.mimuw.students.wosiu.scraper;
 
 import com.opencsv.CSVReader;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -9,18 +11,22 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class BrowserOpener {
+	public static Logger logger = Logger.getLogger("BrowserOpener");
 
 	public static void main(String[] args) throws IOException, URISyntaxException, ConnectionException {
 
+		// log4j init
+		BasicConfigurator.configure();
+
 		CSVReader reader = new CSVReader(new FileReader("empty.csv"), '\t');
-
-
 
 		String [] nextLine;
 		while ((nextLine = reader.readNext()) != null) {
 			// nextLine[] is an array of values from the line
+			String url = nextLine[3];
+			logger.info("Open: " + url);
 			WebDriver driver = new FirefoxDriver();
-			driver.get(nextLine[2]);
+			driver.get(url);
 		}
 
 
