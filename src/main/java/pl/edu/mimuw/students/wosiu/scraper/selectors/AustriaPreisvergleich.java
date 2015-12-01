f5f8bfcb731b81339b18b302274aa94c031de3e9
Maciej Andrearczyk <maciej.andrearczyk@gmail.com>
2015-12-01 02:18:16 +0100

    Austria fix

--- src/main/java/pl/edu/mimuw/students/wosiu/scraper/selectors/AustriaPreisvergleich.java
+++ src/main/java/pl/edu/mimuw/students/wosiu/scraper/selectors/AustriaPreisvergleich.java
@@ -9,7 +9,10 @@ import pl.edu.mimuw.students.wosiu.scraper.delab.DELabProductSelector;
 import pl.edu.mimuw.students.wosiu.scraper.delab.ProductResult;
 
 import java.net.URL;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Algorytm:
@@ -32,35 +35,62 @@ public class AustriaPreisvergleich extends DELabProductSelector {
 	public List<URL> getNextPages(Document document) {
 		document.setBaseUri(getSourceURL().toString());
 
-		String nextStrUrl = null;
 		List<URL> res = new LinkedList<>();
 
-		try {
-			Elements elements = document.getElementsByClass("pag-link");
-			Element next = elements.first().select("a").first();
-			nextStrUrl = next.attr("abs:href");
-			res.add(Utils.stringToURL(nextStrUrl));
-		} catch (ConnectionException e) {
-			logger.debug(e.toString());
-		} catch (NullPointerException e) {
-			logger.warn("npe: " + e.getMessage());
+		final Elements select = document.select("div.article.toshop.toarticle div.float-left.full div.buttonholder a:not([c])");
+		List<Element> elements = Arrays.asList(select.toArray(new Element[select.size()]));
+
+		if (elements.size() > 20) {
+			elements = elements.subList(0, 20);
 		}
+		for (Element element : elements) {
+			try {
+				res.add(Utils.stringToURL(element.attr("abs:href")));
+			} catch (ConnectionException e) {
+				e.printStackTrace();
+			}
+		}
+
 
 		return res;
 	}
 
 	@Override
 	public Object getProducts(Document document) {
+		document.setBaseUri(getSourceURL().toString());
 		List<ProductResult> products = new LinkedList<>();
 
-		if (!document.toString().contains("Ihre Suchanfrage hat keine Ergebnisse erbracht")) {
-			final Elements elements = document.getElementsByClass("article");
+		Elements select = document.select("div.wrapper.gallery div.article");
 
-            Date date = new Date();
-			for (Element element : elements) {
-				products.add(buildProductResult(element, date));
-			}
+		List<Element> elements = Arrays.asList(select.toArray(new Element[select.size()]));
+		if (elements.size() > 20) {
+			elements = elements.subList(0, 20);
 		}
+		Date date = new Date();
+		//1st view
+		for (Element element : elements) {
+			products.add(buildProductResult(element, date));
+		}
+
+		//2nd view
+		elements = document.select("div.subitem div.article");
+		for (Element element : elements) {
+			ProductResult product = new ProductResult();
+			product.setCountry(getCountry());
+			product.setPrice(element.select("div.float-left.price span.highlightprice:not(.smaller) span.units").text() + " EUR");
+			product.setProduct(element.select("div.namedelivery span.larger a[href]").text());
+			product.setSearcher(getSourceURL().toString());
+			product.setShopURL(element.select("div.namedelivery span.larger a[href]").attr("abs:href"));
+			String shop = element.select("div.image img.shoplogo").attr("alt");
+			if (shop == null || shop.isEmpty()) {
+				shop = element.select("div.image span.textshoplogo").text();
+			}
+			product.setShop(shop);
+			product.setTime(date.getTime());
+			product.setProxy(getLastUsedProxy());
+			products.add(product);
+		}
+
 
 		return products;
 	}
@@ -68,10 +98,10 @@ public class AustriaPreisvergleich extends DELabProductSelector {
 	private ProductResult buildProductResult(Element element, Date date) {
 		final ProductResult product = new ProductResult();
 		URL shopURL = getShopURL(element);
-        product.setCountry(getCountry());
+		product.setCountry(getCountry());
 		product.setPrice(getPrice(element));
 		product.setProduct(getProduct(element));
-        product.setSearcher(getSourceURL().toString());
+		product.setSearcher(getSourceURL().toString());
 		product.setShopURL(shopURL.toString());
 		product.setShop(getShopName(element));
 		product.setTime(date.getTime());
@@ -80,21 +110,20 @@ public class AustriaPreisvergleich extends DELabProductSelector {
 	}
 
 	private String getShopName(Element element) {
-		return element.select("a.shoplogo").first().text();
+		return element.select("a.shoplogo").text();
 	}
 
 	private URL getShopURL(Element element) {
 		return followUrl(
-				getSourceURL() +
-						element.getElementsByClass("namedescription").get(0).select("a[href]").get(0).attr("href")
+				element.select("div.namedescription a[href]").attr("abs:href")
 		);
 	}
 
 	private String getProduct(Element element) {
-		return element.getElementsByClass("namedescription").get(0).select("a[href]").get(0).text();
+		return element.select("div.namedescription span.larger a[href]").text();
 	}
 
 	private String getPrice(Element element) {
-		return element.getElementsByClass("price").get(0).getElementsByClass("units").text() + "EUR";
+		return element.select("div.price span.highlightprice span.units").text() + " EUR";
 	}
 }
